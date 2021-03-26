aa90515d11cba467393d7400b0996abe74cd4a8a
skaidevs <skaidevs@gmail.com>
2021-03-26 07:53:37 +0000

    test passed for fetching news item

--- test/news_api_provider_test.dart
+++ test/news_api_provider_test.dart
@@ -13,7 +13,18 @@ void main() {
       return Response(json.encode([1, 2, 3, 4, 5, 6, 7]), 200);
     });
 
+    //expect
     final ids = await newsApi.fetchTopIds();
     expect(ids, [1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  test('fetchIem returns a news Model', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    final item = await newsApi.fetchNewsItem(789);
+    expect(item.id, 123);
   });
 }
